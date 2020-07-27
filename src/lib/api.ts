772fcf3ba7feab8c3ff268b2c8da125457edee68
CyberiumShadow <cyberiumdev@gmail.com>
2020-07-27 09:58:58 +0000

    feat: implement all endpoints with no pagination or filters

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,54 +1,97 @@
 import fetch from 'node-fetch';
 import { ApiURL } from '../util/Constants';
 
+import type { Response } from 'node-fetch';
+
 const headers = {
 	Authorization: `Bearer ${process.env.UP_PAT}`
 }
 
 export const getAllAccounts = async (): Promise<any> => {
+	// TODO: Implement pagination as per API
+	const response: Response = await fetch(`${ApiURL}/accounts`, { headers });
 
+	return response.json();
 }
 
-export const getAccount = async (): Promise<any> => {
+export const getAccount = async (id: string): Promise<any> => {
+	const response: Response = await fetch(`${ApiURL}/accounts/${id}`, { headers });
 
+	return response.json();
 }
 
 export const getAllTransactions = async (): Promise<any> => {
+	// TODO: Implement pagination as per API
+	const response: Response = await fetch(`${ApiURL}/transactions`, { headers });
+
+	return response.json();
 
 }
 
-export const getTransaction = async (): Promise<any> => {
+export const getTransaction = async (id: string): Promise<any> => {
+	const response: Response = await fetch(`${ApiURL}/transactions/${id}`, { headers });
+
+	return response.json();
 
 }
 
-export const getTransactionByAccount = async (): Promise<any> => {
+export const getTransactionByAccount = async (accountID: string): Promise<any> => {
+	// TODO: Implement pagination as per API
+	const response: Response = await fetch(`${ApiURL}/accounts/${accountID}/transactions`, { headers });
 
+	return response.json();
 }
 
 export const getAllWebhooks = async (): Promise<any> => {
+	// TODO: Implement pagination as per API
+	const response: Response = await fetch(`${ApiURL}/webhooks`, { headers });
+
+	return response.json();
 
 }
 
-export const createWebhook = async (): Promise<any> => {
+export const createWebhook = async (url: string): Promise<any> => {
+	const response: Response = await fetch(`${ApiURL}/webhooks`, {
+		method: 'POST', headers, body: JSON.stringify({
+			data: {
+				attributes: {
+					url
+				}
+			}
+		})
+	});
 
+	return response.json();
 }
 
-export const getWebhook = async (): Promise<any> => {
+export const getWebhook = async (webHookID: string): Promise<any> => {
+	const response: Response = await fetch(`${ApiURL}/webhooks/${webHookID}`, { headers });
 
+	return response.json();
 }
 
-export const deleteWebhook = async (): Promise<any> => {
+export const deleteWebhook = async (webHookID: string): Promise<number> => {
+	const response: Response = await fetch(`${ApiURL}/webhooks/${webHookID}`, { headers });
 
+	return response.status;
 }
 
-export const pingWebhook = async (): Promise<any> => {
+export const pingWebhook = async (webHookID: string): Promise<any> => {
+	const response: Response = await fetch(`${ApiURL}/webhooks/${webHookID}/ping`, { method: 'POST', headers });
 
+	return response.json();
 }
 
-export const getWebhookLogs = async (): Promise<any> => {
+export const getWebhookLogs = async (webHookID: string): Promise<any> => {
+	// TODO: Implement pagination as per API
+	const response: Response = await fetch(`${ApiURL}/webhooks/${webHookID}/logs`, { headers });
+
+	return response.json();
 
 }
 
 export const ping = async (): Promise<any> => {
+	const response: Response = await fetch(`${ApiURL}/util/ping`);
 
+	return response.json();
 }
