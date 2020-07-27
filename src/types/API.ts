856a7febf6b07a866041b41082caa97a91e5a438
CyberiumShadow <cyberiumdev@gmail.com>
2020-07-27 16:43:29 +0000

    feat(typings): add typings for API

---
+++ src/types/API.ts
@@ -0,0 +1,26 @@
+import type { AccountResource } from './Account';
+import type { TransactionResource } from './Transaction';
+import type { WebhookResource } from './Webhook';
+
+export interface APIResponse {
+	data?: AccountResource | AccountResource[] | TransactionResource | TransactionResource[] | WebhookResource | WebhookEventResource | WebhookDeliveryLogResource[];
+	errors?: ErrorObject[];
+	meta?: {
+		id: string;
+		statusEmoji: string;
+	}
+	links?: {
+		prev: string | null;
+		next: string | null;
+	}
+}
+
+export interface ErrorObject {
+	status: string;
+	title: string;
+	detail: string;
+	source?: {
+		parameter?: string;
+		pointer?: string;
+	}
+}
